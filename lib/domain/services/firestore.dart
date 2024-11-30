8f64fd1d2de86649f3ee3fdcb3d42e341b6a876e
bmurcia <bnmurci4@gmail.com>
2024-11-30 16:44:07 -0600

    Se agrego boton para nuevo visitante con la funcion Create

---
+++ lib/domain/services/firestore.dart
@@ -0,0 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirestoreService {
+  final CollectionReference _visitsCollection = FirebaseFirestore.instance.collection('visits');
+
+  //CREATE
+  Future<void> addVisit(String name, String dni, String visitReason, String personToVisit, String vehicle, String entryTime, String companions) {
+      return _visitsCollection.add({
+        'name': name,
+        'identification': dni,
+        'visitReason': visitReason,
+        'personToVisit': personToVisit,
+        'transportation': vehicle,
+        'entryTime': Timestamp.now(),
+        'companions': companions,
+      });    
+  }
+
+  //READ
+  Stream<QuerySnapshot> getVisits() {
+    final visitsStream = _visitsCollection.orderBy('entryTime', descending: true).snapshots();
+    return visitsStream;
+  }
+}
--- lib/presentation/screen/home/home_screen.dart
+++ lib/presentation/screen/home/home_screen.dart
@@ -1,7 +1,94 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_crud_visitante/domain/services/firestore.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  final FirestoreService _firestoreService = FirestoreService();
+
+  final nameController = TextEditingController();
+  final dniController = TextEditingController();
+  final visitReasonController = TextEditingController();
+  final personToVisitController = TextEditingController();
+  final vehicleController = TextEditingController();
+  final entryTimeController = TextEditingController();
+  final companionsController = TextEditingController();
+
+  void openVisitBox({String? docId}) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Add Visit'),
+
+          content: SingleChildScrollView(
+            child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: nameController,
+                decoration: const InputDecoration(labelText: 'Nombre'),
+              ),
+              TextField(
+                controller: dniController,
+                decoration: const InputDecoration(labelText: 'DNI'),
+              ),
+              TextField(
+                controller: visitReasonController,
+                decoration: const InputDecoration(labelText: 'Razon de Visita'),
+              ),
+              TextField(
+                controller: personToVisitController,
+                decoration: const InputDecoration(labelText: 'Persona a quien visita'),
+              ),
+              TextField(
+                controller: vehicleController,
+                decoration: const InputDecoration(labelText: 'Vehiculo'),
+              ),
+              TextField(
+                controller: companionsController,
+                decoration: const InputDecoration(labelText: 'Acompañantes'),
+              ),
+            ],
+          ),
+          ),
+          actions: [
+            ElevatedButton(
+              onPressed: (){
+                if (docId == null) {
+                  _firestoreService.addVisit(
+                    nameController.text,
+                    dniController.text,
+                    visitReasonController.text,
+                    personToVisitController.text,
+                    vehicleController.text,
+                    entryTimeController.text,
+                    companionsController.text,
+                  );
+                }
+                nameController.clear();
+                dniController.clear();
+                visitReasonController.clear();
+                personToVisitController.clear();
+                vehicleController.clear();
+                companionsController.clear();
+                Navigator.of(context).pop();
+              }, 
+              
+
+              child: const Text('Add Visit'),
+            )
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,8 +96,11 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Welcome the Visit APP'),
       ),
-      body: ListView(
-        
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          openVisitBox();
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
